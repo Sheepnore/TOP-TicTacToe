0b1794b7941ba2533662b44390b2cdcd1e247ee2
Sheepnore <a83840066@gmail.com>
2024-09-23 23:31:24 +0800

    complete winning condition, disable start button when clicked,

--- index.js
+++ index.js
@@ -1,11 +1,88 @@
 function renderPage(){
-  const gameboard = [
+  let gameboard = [
     '0','1','2',
     '3','4','5',
     '6','7','8',
   ];
 
-  
+  let isGameFinished = false;
+  let isPlayerTwoTurn = false;
+
+  // start button eventlistener
+  const startButton = document.querySelector('.startGame');
+  startButton.addEventListener('click',()=>{
+    startButton.disabled = true; 
+    let players = createPlayers();
+    startGame(players);
+  });
+
+  function winningCondition(playerSym,playerName){
+    if ((gameboard[0]=== playerSym) && (gameboard[1]=== playerSym) && (gameboard[2]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[3]=== playerSym) && (gameboard[4]=== playerSym) && (gameboard[5]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[6]=== playerSym) && (gameboard[7]=== playerSym) && (gameboard[8]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[0]=== playerSym) && (gameboard[3]=== playerSym) && (gameboard[6]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[1]=== playerSym) && (gameboard[4]===playerSym) && (gameboard[7]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[2]=== playerSym) && (gameboard[5]=== playerSym) && (gameboard[8]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[0]=== playerSym) && (gameboard[4]=== playerSym) && (gameboard[8]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+    if ((gameboard[2]=== playerSym) && (gameboard[4]=== playerSym) && (gameboard[6]=== playerSym)){
+      isGameFinished = true;
+      checkGameFinished(playerName)
+      isGameFinished = false;
+      console.log('winning!')
+    }
+  }
+
+  function checkGameFinished(winningPlayer){
+    const winningDialog = document.querySelector('.winningDialog');
+    if (isGameFinished){
+      winningDialog.showModal();
+    }
+    const txtPlayerName = document.querySelector('.playerName');
+    txtPlayerName.innerHTML = `The Winner is: ${winningPlayer}!`;
+    const closeDialogButton = document.querySelector('.closeDialogButton');
+    closeDialogButton.addEventListener('click',()=>{
+      winningDialog.close();
+      isPlayerTwoTurn = false;
+      startButton.disabled = false;
+      startGame();
+    })
+  }
+
   function createPlayers(){
     const playerOneElem = document.querySelector('#playerOne');
     const playerOneName  = playerOneElem.value;
@@ -29,56 +106,51 @@ function renderPage(){
     }
     return {playerOne,playerTwo};
   };
-  
-  let isPlayerTwoTurn = false;
-  let players;
-  const startButton = document.querySelector('.startGame');
-  startButton.addEventListener('click',()=>{
-     players = createPlayers();
-     console.log(players);
-     console.log(players.playerOne.playerOneSym);
-     startGame();
-  });
 
 
   let cells = document.querySelectorAll('.cell');
   cells = [...cells];
 
-  // function disableClickEvent(cell){
-  //   if (cell.innerHTML !== ''){
-  //     cell.removeEventListener('click',handleClick);
-  //   };
-  // };
+  function startGame(players){
+    gameboard = [
+      '0','1','2',
+      '3','4','5',
+      '6','7','8',
+    ];
+    cells.forEach(cell=>{
+      cell.innerHTML = '';
+    })
 
-  function startGame(){
     gameboard.forEach(data=>{
       const dataIndex = gameboard.indexOf(data);
-      console.log(dataIndex);
 
       cells.forEach(cell=>{
         const cellIndex = cells.indexOf(cell);
         if (cellIndex === dataIndex){
           cell.addEventListener('click',function handleClick(){
+            console.log(players);
             if (isPlayerTwoTurn){
+              console.log(players);
               gameboard[cellIndex] = players.playerTwo.playerTwoSym;
               cell.innerHTML = players.playerTwo.playerTwoSym;
               isPlayerTwoTurn = !isPlayerTwoTurn;
+              winningCondition(players.playerTwo.playerTwoSym,players.playerTwo.playerTwoName);
+              console.log(gameboard);
               cell.removeEventListener('click',handleClick);
             }
-            else{
+            else{   
+              console.log(players);
               gameboard[cellIndex] = players.playerOne.playerOneSym;
               cell.innerHTML = players.playerOne.playerOneSym;
               isPlayerTwoTurn = !isPlayerTwoTurn;
+              winningCondition(players.playerOne.playerOneSym,players.playerOne.playerOneName);
+              console.log(gameboard);
               cell.removeEventListener('click',handleClick);
             };
           })
         }
       })
     })
-    
   }
-
-  
-
 };
 renderPage()
